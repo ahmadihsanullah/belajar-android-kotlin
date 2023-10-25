7082a9637573cf22d5998f73d7b881ffe0b3dae4
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-10-25 08:54:10 +0700

    menambah bottom navigation

---
+++ app/src/main/java/belajar/android/dasar/bottomnavigation/BottomNavActivity.kt
@@ -0,0 +1,12 @@
+package belajar.android.dasar.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import belajar.android.dasar.R
+
+class BottomNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+    }
+}
